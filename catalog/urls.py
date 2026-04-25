7cb239663c5ded87b60ea97a8dbc1cba6ca630aa
Trenton Edwards <gage@tg11.org>
2026-04-24 22:34:51 -0400

    add Open Graph, Twitter card, and product oEmbed support

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .views import HomeView, ProductDetailView, ProductListView, StorePageDetailView
+from .views import HomeView, ProductDetailView, ProductListView, ProductOEmbedView, StorePageDetailView
 
 app_name = 'catalog'
 
@@ -8,5 +8,6 @@ urlpatterns = [
     path('', HomeView.as_view(), name='home'),
     path('products/', ProductListView.as_view(), name='product_list'),
     path('products/<slug:slug>/', ProductDetailView.as_view(), name='product_detail'),
+    path('oembed/', ProductOEmbedView.as_view(), name='product_oembed'),
     path('pages/<slug:slug>/', StorePageDetailView.as_view(), name='page_detail'),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,6 +1,26 @@
+from urllib.parse import urlencode, urlparse
+
+from django.conf import settings
+from django.http import HttpResponseBadRequest, JsonResponse
+from django.urls import reverse
+from django.utils.html import strip_tags
+from django.utils.text import Truncator
+from django.views import View
 from django.views.generic import DetailView, ListView, TemplateView
 
 from .models import Product, StorePage
+
+
+def _site_url() -> str:
+    return getattr(settings, 'SITE_URL', 'https://shop.tg11.org').rstrip('/')
+
+
+def _absolute_uri(request, value: str | None) -> str:
+    if not value:
+        return ''
+    if value.startswith('http://') or value.startswith('https://'):
+        return value
+    return request.build_absolute_uri(value)
 
 
 class HomeView(TemplateView):
@@ -10,6 +30,11 @@ class HomeView(TemplateView):
         context = super().get_context_data(**kwargs)
         context['featured_products'] = Product.objects.filter(is_active=True, is_featured=True)[:6]
         context['latest_products'] = Product.objects.filter(is_active=True)[:8]
+        context['meta_title'] = 'TG11 Shop'
+        context['meta_description'] = 'Quality goods, secure checkout, and fast fulfillment from TG11 Shop.'
+        context['meta_url'] = self.request.build_absolute_uri()
+        context['meta_type'] = 'website'
+        context['twitter_card'] = 'summary'
         return context
 
 
@@ -21,6 +46,15 @@ class ProductListView(ListView):
     def get_queryset(self):
         return Product.objects.filter(is_active=True).prefetch_related('variants', 'images')
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['meta_title'] = 'Products | TG11 Shop'
+        context['meta_description'] = 'Browse products available now at TG11 Shop.'
+        context['meta_url'] = self.request.build_absolute_uri()
+        context['meta_type'] = 'website'
+        context['twitter_card'] = 'summary'
+        return context
+
 
 class ProductDetailView(DetailView):
     model = Product
@@ -30,6 +64,29 @@ class ProductDetailView(DetailView):
     def get_queryset(self):
         return Product.objects.filter(is_active=True).prefetch_related('variants', 'images')
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        product = self.object
+
+        raw_description = product.short_description or strip_tags(product.description)
+        description = Truncator(raw_description).chars(180)
+
+        image_obj = product.images.first()
+        image_url = _absolute_uri(self.request, image_obj.image.url if image_obj else '')
+        product_url = self.request.build_absolute_uri(product.get_absolute_url())
+
+        context['meta_title'] = f'{product.name} | TG11 Shop'
+        context['meta_description'] = description
+        context['meta_url'] = product_url
+        context['meta_image'] = image_url
+        context['meta_type'] = 'product'
+        context['twitter_card'] = 'summary_large_image' if image_url else 'summary'
+        context['oembed_url'] = (
+            f"{self.request.build_absolute_uri(reverse('catalog:product_oembed'))}"
+            f"?{urlencode({'url': product_url, 'format': 'json'})}"
+        )
+        return context
+
 
 class StorePageDetailView(DetailView):
     model = StorePage
@@ -38,3 +95,50 @@ class StorePageDetailView(DetailView):
 
     def get_queryset(self):
         return StorePage.objects.filter(is_published=True).prefetch_related('products__variants', 'products__images')
+
+
+class ProductOEmbedView(View):
+    """Serve oEmbed JSON for TG11 product pages."""
+
+    def get(self, request):
+        requested_url = request.GET.get('url', '').strip()
+        if not requested_url:
+            return HttpResponseBadRequest('Missing url parameter.')
+
+        parsed = urlparse(requested_url)
+        path = parsed.path or ''
+        prefix = reverse('catalog:product_list')
+        if not path.startswith(prefix):
+            return HttpResponseBadRequest('URL is not a product URL.')
+
+        slug = path.removeprefix(prefix).strip('/').split('/')[0]
+        if not slug:
+            return HttpResponseBadRequest('Unable to resolve product slug from URL.')
+
+        product = Product.objects.filter(is_active=True, slug=slug).prefetch_related('images').first()
+        if not product:
+            return HttpResponseBadRequest('Product not found for URL.')
+
+        image_obj = product.images.first()
+        thumbnail_url = _absolute_uri(request, image_obj.image.url if image_obj else '')
+        product_url = request.build_absolute_uri(product.get_absolute_url())
+        description = Truncator(product.short_description or strip_tags(product.description)).chars(180)
+
+        payload = {
+            'version': '1.0',
+            'type': 'rich',
+            'provider_name': 'TG11 Shop',
+            'provider_url': _site_url(),
+            'title': product.name,
+            'author_name': 'TG11 Shop',
+            'author_url': _site_url(),
+            'html': f'<a href="{product_url}">{product.name}</a>',
+            'width': 600,
+            'height': 338,
+            'url': product_url,
+            'description': description,
+        }
+        if thumbnail_url:
+            payload['thumbnail_url'] = thumbnail_url
+
+        return JsonResponse(payload)
